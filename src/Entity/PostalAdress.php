415487961e0bab47adb7a41387d4f09622c95d43
Albert Arques <albertarques@gmail.com>
2023-10-06 15:40:35 +0200

    Added PostalAdress entity & relationship with Client entity & Timestampable attributes

---
+++ src/Entity/PostalAdress.php
@@ -0,0 +1,150 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\PostalAdressRepository;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: PostalAdressRepository::class)]
+class PostalAdress
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\ManyToOne(inversedBy: 'postalAdresses')]
+    #[ORM\JoinColumn(nullable: false)]
+    private ?Client $client_id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $street_adress = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $town = null;
+
+    #[ORM\Column(length: 20)]
+    private ?string $postal_code = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $province = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $country = null;
+
+    /**
+     * @var \DateTime
+     */
+    #[Gedmo\Timestampable(on: 'create')]
+    #[ORM\Column(name: 'created', type: Types::DATETIME_MUTABLE)]
+    private ?\DateTimeInterface $created = null;
+
+    /**
+     * @var \DateTime
+     */
+    #[ORM\Column(name: 'updated', type: Types::DATETIME_MUTABLE)]
+    #[Gedmo\Timestampable]    
+    private ?\DateTimeInterface $updated = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getClientId(): ?Client
+    {
+        return $this->client_id;
+    }
+
+    public function setClientId(?Client $client_id): static
+    {
+        $this->client_id = $client_id;
+
+        return $this;
+    }
+
+    public function getStreetAdress(): ?string
+    {
+        return $this->street_adress;
+    }
+
+    public function setStreetAdress(string $street_adress): static
+    {
+        $this->street_adress = $street_adress;
+
+        return $this;
+    }
+
+    public function getTown(): ?string
+    {
+        return $this->town;
+    }
+
+    public function setTown(string $town): static
+    {
+        $this->town = $town;
+
+        return $this;
+    }
+
+    public function getPostalCode(): ?string
+    {
+        return $this->postal_code;
+    }
+
+    public function setPostalCode(string $postal_code): static
+    {
+        $this->postal_code = $postal_code;
+
+        return $this;
+    }
+
+    public function getProvince(): ?string
+    {
+        return $this->province;
+    }
+
+    public function setProvince(string $province): static
+    {
+        $this->province = $province;
+
+        return $this;
+    }
+
+    public function getCountry(): ?string
+    {
+        return $this->country;
+    }
+
+    public function setCountry(string $country): static
+    {
+        $this->country = $country;
+
+        return $this;
+    }
+
+    public function getCreated(): ?\DateTimeInterface
+    {
+        return $this->created;
+    }
+
+    public function setCreated(\DateTimeInterface $created): static
+    {
+        $this->created = $created;
+
+        return $this;
+    }
+
+    public function getUpdated(): ?\DateTimeInterface
+    {
+        return $this->updated;
+    }
+
+    public function setUpdated(\DateTimeInterface $updated): static
+    {
+        $this->updated = $updated;
+
+        return $this;
+    }
+}
