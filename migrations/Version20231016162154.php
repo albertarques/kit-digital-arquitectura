68b35db997e84e837887e6cdc970a99deed430e4
Albert Arques <albertarques@gmail.com>
2023-10-16 18:27:42 +0200

    Updated migration

---
+++ migrations/Version20231016162154.php
@@ -0,0 +1,33 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20231016162154 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE camera_roll_type ADD created DATETIME NOT NULL, ADD updated DATETIME DEFAULT NULL');
+        $this->addSql('ALTER TABLE postal_adress CHANGE updated updated DATETIME DEFAULT NULL');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE camera_roll_type DROP created, DROP updated');
+        $this->addSql('ALTER TABLE postal_adress CHANGE updated updated DATETIME NOT NULL');
+    }
+}
